d6bf6373204762a5ce59e606a250e0e1822c7480
Desmond Harris <desmondharris912@gmail.com>
2020-02-22 01:29:41 -0600

    Updates

---
+++ precache-manifest.35ae798bdc00144c88e9b3995b28d454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8006939db0190d7770fa4b7c850a6ab8",
+    "url": "/WikiViewer/index.html"
+  },
+  {
+    "revision": "46df22f14ba452817a5c",
+    "url": "/WikiViewer/static/css/main.39909302.chunk.css"
+  },
+  {
+    "revision": "aac2a29b3c92ebd48100",
+    "url": "/WikiViewer/static/js/2.46aac141.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WikiViewer/static/js/2.46aac141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46df22f14ba452817a5c",
+    "url": "/WikiViewer/static/js/main.21e3054a.chunk.js"
+  },
+  {
+    "revision": "d59013859ba2d6ecbde0",
+    "url": "/WikiViewer/static/js/runtime-main.3c2d3caa.js"
+  }
+]);
